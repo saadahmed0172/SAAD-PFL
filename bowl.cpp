6f83ab7d3099369214bdd28b4c21fe3730880ba7
saadahmed0172 <saadahmed0172@gmail.com>
2026-02-22 09:37:03 +0500

    message

---
+++ bowl.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+main() {
+
+system("color 07");
+cout<<"                             *         *         *                          "<<endl;
+cout<<"                               *         *         *                            "<<endl;
+cout<<"                                 *         *         *                          "<<endl;
+cout<<"                                *         *         *                               "<<endl;
+cout<<"                              *         *         *                                      "<<endl;
+cout<<"                               *         *         *                                        "<<endl;
+cout<<"                                *         *         *                                          "<<endl;
+cout<<"                      ******************************************           "<<endl;
+cout<<"                       *                                      *              "<<endl;
+cout<<"                        *                                    *                 "<<endl;
+cout<<"                         *                                  *                       "<<endl;
+cout<<"                          *                                *                        "<<endl;
+cout<<"                           *                              *                             "<<endl;
+cout<<"                            *                            *                                      "<<endl;
+cout<<"                             ****************************                                      "<<endl;
+cout<<"                             #WELCOME TO FOODY APPLICATION                "<<endl;
+}
+                  
